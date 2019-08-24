eeae6178bc815ae7f6f84b067c4278cf343bb10d
donxiya <31514669+donxiya@users.noreply.github.com>
2019-08-23 20:25:26 -0500

    css update

--- models/note.js
+++ models/note.js
@@ -1,24 +1,17 @@
 const mongoose = require('mongoose');
 
-// create Schema class
 const Schema = mongoose.Schema;
 
-// create note schema
 const NoteSchema = new Schema({
-  // title is a required string
   text: {
     type: String,
     required: true
   },
-  // date is set when added to database
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-// create the Note model with the NoteSchema
 const Note = mongoose.model("Note", NoteSchema);
-
-// export the model
 module.exports = Note;
